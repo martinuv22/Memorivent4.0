dec124b7a1ab4aac4c1ac133c4c1a3d70690810f
Martin Utrera <utrera.martin90@gmail.com>
2023-04-15 01:27:15 -0300

    mejora trabajo

--- back.js
+++ back.js
@@ -12,29 +12,46 @@ stock (número de piezas disponibles)*/
 /*Debe contar con un método “addProduct” el cual agregará un producto al arreglo de productos inicial.
 Todos los campos son obligatorios menos id que debe agregarse automáticamente  y auto- incrementable*/
 //Debe contar con un método “getProducts” el cual debe devolver el arreglo con todos los productos creados hasta ese momento
+/*
+Te dejo algunas recomendaciones generales:
 
+Deberías tener en cuenta la indentación de tu código, para que sea más legible. Ahora son pocas líneas, pero cuando el proyecto crece es importante que el código sea legible.
+
+También te recomiendo que uses nombres de variables más descriptivos, por ejemplo, en lugar de usar “uno” podrías usar “productFound”.
+
+
+
+En el método addProduct, podrías usar el operador ternario para asignar el valor de id, en lugar de usar un if/else. Eso te lo simplificaría a una sola línea, y te ahorraría la declaración de la variable id. Por ejemplo:
+
+let id = this.product.length === 0 ? 1 : this.product[this.product.length-1].id + 1
+
+También es conveniente que el método devuelva algo, por ejemplo, el producto agregado si todo salió bien, o un error, por ejemplo si faltan campos.
+
+Te faltó validar que todos los campos sean obligatorios.
+
+Cuidado con los nombres de las variables, estás usando “event” en lugar de “product”, lo mismo en el método getProductById.
+
+
+
+En el método getProductById, también podrías usar el operador ternario, Por ejemplo:
+
+let productFound = this.product.find(each=> each.id === event_id)
+
+productFound ? console.log(productFound) : console.log('Not found')
+
+De igual manera que en el método addProduct, podrías devolver el producto encontrado, o un error si no se encontró.
+
+
+
+Por lo demás está muy bien. Dejo la pr abierta por si queres hacer los cambios que te comenté antes de hacer el merge.
+
+Un saludo!*/
 class ProductManager{
 
     constructor(){
-
     
     this.product= []
-    
-    }
-    
-    addProduct({title,description,price,thumbnail,stock}){
 
-        let id 
-        if(this.product.length===0){
-            id =1
-        }else{
-           let lastProduct = this.product[this.product.length-1]
-           id=lastProduct.id+1
-       console.log(id)
-        }
-        let event = {title,description,price,thumbnail,stock,id}
-        this.product.push(event)
-    
     }
     getProducts(){
 
@@ -42,17 +59,20 @@ class ProductManager{
            return this.product
                
            }
-    getProductById(event_id){
+    addProduct({title,description,price,thumbnail,stock}){
+       let id =this.product.length===0? 1 :this.product[this.product.length-1].id+1 
+       console.log(id)
+       let product={title,description,price,thumbnail,stock,id}
+       product=(title ===''|| description ==='' ||price === '' ||thumbnail === ''||stock ==='')?console.log("Debes completar todos los campos"): this.product.push(product)
+       return product
 
-        let uno = this.product.find(each=> each.id === event_id)
-        if(uno){
-            console.log(uno)
-            return uno
-        }else{
-console.log('Not found')
-
-        }
-
+    }
+  
+    getProductById(product_id){
+        let productFound = this.product.find(each=> each.id === product_id)
+        productFound ?console.log(productFound)  :console.log('Not found')
+        return productFound
+     
     }
 
     }
@@ -64,3 +84,4 @@ console.log('Not found')
     producto.getProductById(2)
     producto.getProductById(5)
 
+ 
